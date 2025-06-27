53ea27dc784524935629c6cf391f59f2888b1e14
MichelPT <michelpt76767@gmail.com>
2025-06-27 14:34:10 +0700

    added app main

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # A simple route to confirm the API is running
+    return "Flask backend is running!"
+
+
+if __name__=="__main__":
+    app.run()
